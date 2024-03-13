7608a339b955a0c88b25b0eb8c3034991fbdba8b
Vishal Balekundri <vishalbalekundri88@gmail.com>
2024-03-13 16:56:04 +0530

    Constants update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,11 +6,11 @@ export const YOUTUBE_IMG =
 export const USER_ICON =
   "https://banner2.cleanpng.com/20180604/cio/kisspng-user-profile-avatar-computer-icons-google-account-5b1571035447a7.6413663015281318433452.jpg";
 
-const YOUTUBE_API_KEY = "AIzaSyAjuE8h-BFugesiiwQ9rIFwqzQyUrp7_Z0";
+
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
-  YOUTUBE_API_KEY;
+  process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const SUGGESTIONS_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
